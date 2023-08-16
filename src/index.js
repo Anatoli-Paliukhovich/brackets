457448b9d71969346abbe652947831a667b28988
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-16 19:20:43 +0300

    feat: sort out the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	bracketsConfig = [];
+	for (let key of str) {
+		if (key === '(' || key === '{' || key === '[') {
+			bracketsConfig.push(key);
+		} else if (key === ')') {
+			if (bracketsConfig.pop() !== '(') {
+				return false;
+			}
+		} else if (key === '}') {
+			if (bracketsConfig.pop() !== '{') {
+				return false;
+			}
+		} else if (key === ']') {
+			if (bracketsConfig.pop() !== '[') {
+				return false;
+			}
+		}
+	}
+	return bracketsConfig.length === 0;
 }
